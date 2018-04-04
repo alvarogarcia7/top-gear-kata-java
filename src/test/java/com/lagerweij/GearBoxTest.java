badf3646413bfae496a850b5d3b438f1b66f04cc
Alvaro Garcia <derecerca@gmail.com>
2018-04-04 18:59:33 +0200

    Ignored, Green: Discovered defect - the gear box does not shift down

--- src/test/java/com/lagerweij/GearBoxTest.java
+++ src/test/java/com/lagerweij/GearBoxTest.java
@@ -2,6 +2,7 @@ package com.lagerweij;
 
 import org.hamcrest.core.Is;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertThat;
@@ -15,35 +16,63 @@ public class GearBoxTest {
 
     @Test
     public void over_the_upper_bound_it_shifts_gear_up() {
-        shiftTo(1);
-        shiftTo(2);
-        shiftTo(3);
-        shiftTo(4);
-        shiftTo(5);
-        shiftTo(6);
-        shiftTo(6);
+        shiftUpTo(1);
+        shiftUpTo(2);
+        shiftUpTo(3);
+        shiftUpTo(4);
+        shiftUpTo(5);
+        shiftUpTo(6);
+        shiftUpTo(6);
+    }
+
+    @Test
+    @Ignore("Discuss with business whether it should shift down or not")
+    public void below_the_lower_bound_it_shifts_gear_down() {
+        shiftUpToThenDescendOneTo(1);
+        shiftUpToThenDescendOneTo(2);
+        shiftUpToThenDescendOneTo(3);
+        shiftUpToThenDescendOneTo(4);
+        shiftUpToThenDescendOneTo(5);
+        shiftUpToThenDescendOneTo(6);
+        shiftUpToThenDescendOneTo(6);
+    }
+
+    private void shiftUpToThenDescendOneTo(int gearNumber) {
+        final TestableGearBox gearBox = gearBoxReadingGear(gearNumber + 1);
+        shiftDown(gearBox, 1);
+        assertThat(gearBox.getGear(), Is.is(gearNumber));
     }
 
     @Test
     public void cannot_shift_up_from_the_uppest_gear() {
-        final TestableGearBox gearBox = new TestableGearBox();
-        shiftUp(gearBox, 6);
+        final TestableGearBox gearBox = gearBoxReadingGear(6);
         final int gear = gearBox.getGear();
         assertThat(gear, Is.is(6));
         shiftUp(gearBox, 1);
         assertThat(gear, Is.is(6));
     }
 
-    private void shiftTo(int gearNumber) {
-        final TestableGearBox gearBox = new TestableGearBox();
-        shiftUp(gearBox, gearNumber);
+    private void shiftUpTo(int gearNumber) {
+        final TestableGearBox gearBox = gearBoxReadingGear(gearNumber);
         final int gear = gearBox.getGear();
         assertThat(gear, Is.is(gearNumber));
+    }
+
+    private TestableGearBox gearBoxReadingGear(int gearNumber) {
+        final TestableGearBox gearBox = new TestableGearBox();
+        shiftUp(gearBox, gearNumber);
+        return gearBox;
     }
 
     private void shiftUp(TestableGearBox gearBox, int times) {
         for (int i = 0; i < times; i++) {
             gearBox.doit(2500);
+        }
+    }
+
+    private void shiftDown(TestableGearBox gearBox, int times) {
+        for (int i = 0; i < times; i++) {
+            gearBox.doit(400);
         }
     }
 
